cc6277dd7fea7c685b0bdd974ff1273a64cf596b
Øystein Myrmo <oystein.myrmo@gmail.com>
2024-12-29 21:05:38 +0100

    Add initial error reporting

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,61 @@ fn usage() {
     println!("Usage: lox <path to script>");
 }
 
-fn run(source: &String) {
+fn report(line: u32, location: String, message: String) {
+    eprintln!("[line {}] Error{}: {}", line, location, message);
+}
+
+unsafe fn error(line: u32, message: String) {
+    report(line, "".into(), message);
+    HAD_ERROR = true;
+}
+
+unsafe fn run(source: &String) {
     let lines = source.lines();
     for line in lines {
         println!("{}", line);
+    }
+
+    if HAD_ERROR {
+        std::process::exit(65);
     }
 }
 
 fn run_file(path: &String) {
     match std::fs::read_to_string(path) {
-        Ok(source) => {
+        Ok(source) => unsafe {
             run(&source);
-        }
+        },
         Err(e) => {
-            println!("Error reading file {:?}: {}", path, e);
+            eprintln!("Error reading file {:?}: {}", path, e);
             std::process::exit(-1);
         }
     }
 }
 
 fn run_prompt() {
-    let mut buffer = String::new();
-    buffer.reserve(128); // Assume max 128 characters per line initially.
+    let mut line = String::new();
+    line.reserve(128); // Assume max 128 characters per line initially.
 
     loop {
-        buffer.clear();
+        line.clear();
         print!("> ");
-        match std::io::stdin().read_line(&mut buffer) {
-            Ok(_) => {
-                run(&buffer);
-            }
+        match std::io::stdin().read_line(&mut line) {
+            Ok(_) => unsafe {
+                run(&line);
+                HAD_ERROR = false;
+            },
             Err(e) => {
-                println!("Error: {}", e);
+                eprintln!("Error: {}", e);
                 break;
             }
         }
     }
 }
+
+// Note: This should be moved into the actual lexer later on. Following the book for now.
+// Note: When moving this, all the related unsafes can be removed too.
+static mut HAD_ERROR: bool = false;
 
 fn main() {
     // Skip first argument since it is the path to the executable itself.
